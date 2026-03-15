48a1c40bd5bb3b0b64239453892ce9435b74420b
Sai Kumar Gundeboina <75746273+Sai-Kumar-Gundeboina@users.noreply.github.com>
2026-03-15 11:58:07 +0000

    upadte crud.py

--- backend-test/crud.py
+++ backend-test/crud.py
@@ -2,3 +2,17 @@ import models
 from sqlalchemy.orm import Session
 import schemas
 
+def create_user(db: Session, user: schemas.UserCreate):
+    new_user = models.User(
+        name = user.name,
+        email = user.email,
+        password = user.password
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
+
+def get_user_by_email(db: Session, email:str):
+    return db.query(models.User).filter(models.User.email== email).first()
